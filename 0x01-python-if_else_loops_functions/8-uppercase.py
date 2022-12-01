66f624f7e8f745af6806b13655655ca13c18d188
Iyanu <iyanuajimobi12@gmail.com>
2022-11-30 21:08:18 -0800

    Uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str1):
+    new = ""
+    for c in str1:
+        if ord(c) in range(97, 123):
+            new += chr(ord(c)-32)
+        else:
+            new += c
