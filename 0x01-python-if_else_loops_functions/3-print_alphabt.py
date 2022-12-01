f628483ee71a0d3a1f2d89cdd750515ca17fde40
Iyanu <iyanuajimobi12@gmail.com>
2022-11-30 19:39:51 -0800

    no q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) != "e" or chr(i) != "q":
+    if chr(i) == "e" or chr(i) == "q":
+        pass
+    else:
         print("{}".format(chr(i)), end="")
