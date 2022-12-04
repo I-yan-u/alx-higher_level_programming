b91f847962d0114ccd205c80a30c89ef2433399b
Iyanu <iyanuajimobi12@gmail.com>
2022-12-04 01:06:47 -0800

    removed the const and some other updates

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * check_cycle - Checks if a  linked list has a cycle.
@@ -7,9 +7,9 @@
  * Return: Returns 1 if true, Returns 0 if false.
  */
 
-int check_cycle(const listint_t *h)
+int check_cycle(listint_t *h)
 {
-    const listint_t *t1node, *t2node;
+    listint_t *t1node, *t2node;
     unsigned int n = 1;
 
     t1node = h;
@@ -19,7 +19,6 @@ int check_cycle(const listint_t *h)
     {
         t1node = t1node->next;
         t2node = t2node->next->next;
-        //printf("--%d--\n", t1node->n);
         if(t1node == t2node)
         {
             return (1);
