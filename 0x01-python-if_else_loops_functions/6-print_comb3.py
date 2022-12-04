7891770fff4a2515bbffa3833cecaaf32eee886a
Iyanu <iyanuajimobi12@gmail.com>
2022-12-04 14:10:05 -0800

    combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j), end='')
+            print("{:d}{:d}".format(i, j))
         else:
             print("{:d}{:d}".format(i, j), end=', ')
