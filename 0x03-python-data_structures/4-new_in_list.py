7d9971c0fce206bbf8ae22fa0acd51824f37c955
Iyanu <iyanuajimobi12@gmail.com>
2022-12-06 14:54:25 -0800

    Create and replace

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if idx >= len(new_list) or idx < 0:
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
