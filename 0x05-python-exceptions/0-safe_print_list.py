054cbd4fbecedc4ddd8294df96f5aeb415400059
Iyanu <iyanuajimobi12@gmail.com>
2022-12-19 13:13:23 -0800

    Safe print

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for num in my_list[:x]:
+            count += 1
+            print("{}".format(num), end="")
+    except IndexError:
+        pass
+    print()
+    return count
