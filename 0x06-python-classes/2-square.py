c24527badf17b48dba05057290a4964ec98e2d08
Iyanu <iyanuajimobi12@gmail.com>
2022-12-23 09:18:41 -0800

    Class with exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module contains: class Square """
+
+
+class Square():
+    """
+        Square: defines a square.
+        Attributes:
+            size (no type or value identification): size of square.
+        Method:
+            __init__ : init of square class with each attribute.
+    """
+
+    def __init__(self, size=0):
+        """
+            Initialization of attributes to instances.
+            Args:
+                size (no type): size of square.
+        """
+        if (isinstance(size, int)):
+            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
