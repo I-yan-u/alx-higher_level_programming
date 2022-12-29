7ba20eea43b123a7fa6cdc688bfb3e4821ec5fb3
Iyanu <iyanuajimobi12@gmail.com>
2022-12-29 14:32:38 -0800

    Compare squares

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -21,7 +21,7 @@ class Square():
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
@@ -48,6 +48,12 @@ class Square():
         self.__size = value
 
     def area(self):
+        """
+            Get area of square.
+
+            return:
+                area of square.
+        """
         return self.__size * self.__size
 
     def __eq__(self, square):
