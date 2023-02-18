d6b257ff270bf1686eef43b5f11d6a484ef87c4e
Iyanu <iyanuajimobi12@gmail.com>
2023-02-18 04:49:46 -0800

    corrected doc style

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,7 +1,6 @@
-/*
-Script to print out name and score
-showing the highes score first
-*/
+--Script to print out name and score
+--showing the highes score first
+
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
