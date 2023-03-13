3979212560ef0a84b211906e781029f72807e01f
Iyanu <iyanuajimobi12@gmail.com>
2023-03-13 15:17:00 -0700

    Js warm up

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const c_lang = 'C is fun';
+const python = 'Python is cool';
+const js_lang = 'JavaScript is amazing';
+
+console.log(c_lang);
+console.log(python);
+console.log(js_lang);
