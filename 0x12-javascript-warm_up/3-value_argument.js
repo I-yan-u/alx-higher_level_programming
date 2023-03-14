4d07380ce6cb70773dc1497a61fa176079273373
Iyanu <iyanuajimobi12@gmail.com>
2023-03-14 16:01:00 -0700

    fixed typo err

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 if (process.argv.length <= 2 || process.argv[2] === undefined) {
-  console.log('No Argument');
+  console.log('No argument');
 } else {
   console.log(process.argv[2]);
 }
