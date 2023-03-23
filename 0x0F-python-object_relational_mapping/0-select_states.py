f37cd1c0d0591a432903d444a1e30f66a1e531a6
I-yan-u <iyanuajimobi12@gmail.com>
2023-03-23 23:35:35 +0100

    MySQL python query

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script displays states in database by ascending id order
+   Script should take 3 arguments:
+   mysql username, mysql password and database name
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) > 3:
+        link = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3])
+
+        curs = link.cursor()
+        curs.execute("""SELECT * FROM states""")
+        for row in curs.fetchall():
+            print(row)
+        curs.close()
+        link.close()
