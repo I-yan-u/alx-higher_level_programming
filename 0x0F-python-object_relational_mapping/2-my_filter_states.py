b6da2a7de1045d232699fe4d577fd726ae3b92a5
I-yan-u <iyanuajimobi12@gmail.com>
2023-03-25 22:03:39 +0100

    Argument to select

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                                passwd=sys.argv[2], db=sys.argv[3])
 
         curs = link.cursor()
-        query("SELECT * FROM states WHERE states = {} ORDER BY states.id ASC")
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
         curs.execute(query.format(sys.argv[4]))
         for row in curs.fetchall():
             print(row)
