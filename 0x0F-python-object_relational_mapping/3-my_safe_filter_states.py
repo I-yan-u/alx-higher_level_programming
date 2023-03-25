303ea282cd50a9b7b71a10dc10f0cc577138bdc8
I-yan-u <iyanuajimobi12@gmail.com>
2023-03-25 22:45:05 +0100

    Safer way to make queries

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Script displays states in database by ascending id order
+   Where state == arg[4]
+   Script should take 4 arguments:
+   mysql username, mysql password and database name
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) > 3:
+        link = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3])
+
+        curs = link.cursor()
+        query = "SELECT * FROM states ORDER BY states.id ASC"
+        curs.execute(query)
+        for row in curs.fetchall():
+            if row[1] == sys.argv[4]:
+                print(row)
+        curs.close()
+        link.close()
