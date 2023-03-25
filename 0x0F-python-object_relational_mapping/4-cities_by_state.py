4c41b7377df24f5a09b1ff7906981641aabcac95
I-yan-u <iyanuajimobi12@gmail.com>
2023-03-25 23:32:25 +0100

    cities and states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ import sys
 
 if len(sys.argv) > 3:
     link = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                       passwd=sys.argv[2], db=sys.argv[3])
+                           passwd=sys.argv[2], db=sys.argv[3])
 
     cur = link.cursor()
     query = "SELECT cities.id, cities.name, states.name FROM cities \
