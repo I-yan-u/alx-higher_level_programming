eda6e2841cc851f80dda2a107907f1c63df36ea5
I-yan-u <iyanuajimobi12@gmail.com>
2023-03-26 20:58:31 +0100

    delete rows in db

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    ORM; using sql objects as python objects
+    State class
+    Base = declarative_base()
+"""
+
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 3:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    point = session.query(State).filter(
+        State.name.ilike('%a%')).order_by(State.id)
+    session.delete(point)
+    session.commit()
+    print("Not found")
