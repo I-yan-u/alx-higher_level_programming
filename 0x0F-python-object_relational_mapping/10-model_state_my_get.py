b19f3b4ba93f0f4b00a19626ffe5ca1f0aa50523
I-yan-u <iyanuajimobi12@gmail.com>
2023-03-26 19:17:40 +0100

    queries with the alchemist

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    ORM; using sql objects as python objects
+    State class
+    Base = declarative_base()
+"""
+
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 3:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for row in session.query(State).order_by(State.id):
+        if row.name == sys.argv[4]:
+            print("{}".format(row.id))
+        else:
+            print("Not found")
