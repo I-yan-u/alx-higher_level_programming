31b4594cce3fedebff826b32b8506544836377d0
I-yan-u <iyanuajimobi12@gmail.com>
2023-03-31 00:15:37 +0100

    POST request with parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request with parameters
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sXL POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
