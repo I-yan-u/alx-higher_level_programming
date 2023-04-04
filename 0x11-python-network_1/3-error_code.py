6ab944eb854fe1aef40efde73e9c06e2482ed1e9
I-yan-u <iyanuajimobi12@gmail.com>
2023-04-05 00:09:34 +0100

    Error Handling

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Requests from add provided at sys.argv[1].
+Handles HTTP errors
+"""
+import urllib.request as ur
+import urllib.error as ue
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = ur.Request(url, data)
+
+    try:
+        with ur.urlopen(req) as response:
+            print(response.read().decode(encoding="utf-8"))
+    except ue.HTTPError as e:
+        print("Error code: {}".format(e.code))
