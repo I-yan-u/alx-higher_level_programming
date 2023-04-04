df84075a77f69da9f3a47863f62f32ededc21ff7
I-yan-u <iyanuajimobi12@gmail.com>
2023-04-04 20:03:26 +0100

    Request from hoberton

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import urllib.request as ur
+
+
+if __name__ == "__main__":
+    req = ur.Request('https://alx-intranet.hbtn.io/status')
+    with ur.urlopen(req) as response:
+        cont = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(cont)))
+        print("\t- content: {}".format(cont))
+        print("\t- utf8 content: {}".format(cont.decode("utf-8")))
