947e9c225c3ad5e6482553eabac71a571fba7a2d
I-yan-u <iyanuajimobi12@gmail.com>
2023-04-05 00:47:54 +0100

    get header details

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Get address details for address in sys.argv[1]
+Using requests
+"""
+import requests as re
+import sys
+
+
+if __name__ == "__main__":
+    response = re.get(sys.argv[1])
+
+    print(dict(response.headers).get("X-Request-Id"))
