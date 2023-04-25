50509043737ef3d0b6d9d7a1fbef2ea5657a2ba3
I-yan-u <iyanuajimobi12@gmail.com>
2023-04-25 23:34:44 +0100

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const file = require('fs');
-file.readFile(process.argv[2], 'utf8', function (error, content){
+file.readFile(process.argv[2], 'utf8', function (error, content) {
 	console.log(error || content);
 });
