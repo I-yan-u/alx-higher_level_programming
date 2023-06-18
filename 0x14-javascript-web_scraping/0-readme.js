f8150a9977b2e3f2b3134422149aee21c197ec85
Iyanu Ajimobi <107860450+I-yan-u@users.noreply.github.com>
2023-06-18 17:38:43 +0100

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const file = require('fs');
-file.readFile(process.argv[2], 'utf8', function (error, content) {
-	console.log(error || content);
-});
--- 0x14-javascript-web_scraping/test_readme.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/nodejs
-
-const file = require('fs');
-
-const print = function (error, content){console.log(error || content);};
-const content = file.readFile(process.argv[2], 'utf8', print);
