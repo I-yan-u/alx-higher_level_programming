1077a6c14967211b53160a512a53086ac32c4247
I-yan-u <iyanuajimobi12@gmail.com>
2023-07-25 23:36:08 +0100

    request api data

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const r = require('request');
+
+const Id = process.argv[2]; 
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+r(url + Id, (err, response, body) => {
+	if (err !== null) {
+		console.log(err);
+	}
+	console.log(JSON.parse(body).title);
+});
